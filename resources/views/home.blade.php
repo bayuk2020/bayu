3be42e18facab552c056d7b775e953a4d8d7120e
Bayu Kristianto <bayuk2020@gmail.com>
2022-01-07 21:02:42 +0700

    tes update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,4 @@
 <h1>Halaman Home</h1>
 <p>Selamat Datang</p>
 
-<span>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ratione voluptatum, error tenetur quibusdam repellendus, necessitatibus sunt harum maxime mollitia dolores temporibus eos reprehenderit quaerat earum ab exercitationem cumque veniam, porro ut adipisci obcaecati fugiat? Quis voluptatem accusamus corrupti dolorem sunt cupiditate, aut id possimus, exercitationem voluptas ut a quod molestiae. Recusandae veniam est mollitia vitae expedita ipsa quo sunt! Minima laudantium maxime molestias praesentium alias dolores error ea fugit harum laboriosam distinctio ullam, earum laborum beatae enim autem aspernatur temporibus asperiores commodi aliquid vero animi esse excepturi at. Similique, 
-    
-    molestiae rem non eligendi, inventore corrupti at ipsa beatae tempora, vitae sapiente saepe sed a repudiandae autem dolorum cupiditate quod. Laborum, sunt. Quidem, dicta? Deserunt blanditiis nesciunt culpa, amet esse, alias pariatur quidem velit repellendus beatae odio molestias impedit ad. Iure voluptatem voluptatum saepe sed placeat doloribus quibusdam tempora quasi perferendis. Velit at tempore, esse iusto nesciunt quia, facilis deleniti vitae quibusdam fugit fugiat perspiciatis voluptas praesentium dolorem! Quae debitis vitae ad neque eveniet voluptatem obcaecati excepturi libero, est cupiditate deleniti facere possimus culpa architecto. Ex, blanditiis ducimus animi inventore, voluptatum sed magnam aperiam quae accusamus ea saepe ab! Dignissimos, a aliquam? Ea saepe sapiente odit sint facere neque cum? 
-    
-    Alias magnam totam beatae maiores, nam iste, laudantium similique est suscipit voluptatem amet ipsa quis impedit ipsam voluptate dolor aut debitis illum itaque. Magni aut ea praesentium nam dolorem libero corrupti aspernatur tempore autem iste, error recusandae sunt unde natus fuga architecto. Quaerat quam commodi impedit nulla aliquam nisi tempore ratione illo quod, nesciunt doloremque provident voluptatum earum sed sequi excepturi similique temporibus fugiat magni! Voluptas ab accusantium debitis consequuntur. Ex eius facilis vel similique neque ad animi itaque perferendis modi autem assumenda, ullam officia obcaecati numquam nostrum. Voluptatum fugit nemo praesentium qui animi dignissimos ad molestias nesciunt amet natus?
-</span>
-
 @endsection
